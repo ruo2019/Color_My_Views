c492758d50d8379038bf66a78a3684b03e5579e6
ruo20 <ruo2019@outlook.com>
2021-04-14 10:15:27 -0700

    update

--- app/src/main/java/com/conkermobile/colormyviews/MainActivity.kt
+++ app/src/main/java/com/conkermobile/colormyviews/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         when (view.id) {
             R.id.box_one_text -> view.setBackgroundColor(Color.RED)
             R.id.box_two_text -> view.setBackgroundColor(Color.rgb(247, 112, 27))
-            R.id.box_three_text -> view.setBackgroundColor(Color.YELLOW)
+            R.id.box_three_text -> view.setBackgroundColor(Color.rgb(245, 212, 0))
             R.id.box_four_text -> view.setBackgroundColor(Color.GREEN)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_six_text -> view.setBackgroundColor(Color.rgb(183, 0, 255))
